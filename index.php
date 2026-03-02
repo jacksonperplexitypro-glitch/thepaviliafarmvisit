067c2826c75666de060fe1c706fb4a35726061c2
ct-jacksonchin <164849641+ct-jackonchin@users.noreply.github.com>
2026-03-02 15:05:38 +0800

    123

---
+++ index.php
@@ -0,0 +1,24 @@
+
+
+ 
+ 
+<meta name="description" content="全新港鐵大圍站上蓋大型住宅項目 — 新世界crossover港鐵，匠心築造地標豪宅 雙鐵交匯、城門河景、65萬呎商場！ The Pavilia Farm 柏傲莊 III，550間 馬年賀歲登場 65 萬呎商場「圍方 The Wai」 大圍站上蓋 ‧ 交通樞紐。">
+    <meta property="og:description" content="全新港鐵大圍站上蓋大型住宅項目 — 新世界crossover港鐵，匠心築造地標豪宅 雙鐵交匯、城門河景、65萬呎商場！ The Pavilia Farm 柏傲莊 III，550間 馬年賀歲登場 65 萬呎商場「圍方 The Wai」 大圍站上蓋 ‧ 交通樞紐。">
+
+        <meta property="og:image" content="./image/imageyo.jpg">
+ 
+
+        <!-- Required OG Tags for correct display -->
+    <meta property="og:title" content="柏傲莊 III | The Pavilia Farm">
+    <meta property="og:url" content="http://thepaviliafarmhk.com?">
+    <meta property="og:type" content="website">
+
+<script type="text/javascript">
+    // 立即偵測並跳轉
+    if (/Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+        // 使用 location.replace 確保用戶點擊「後退」時不會回到 Web 版觸發無限跳轉
+        window.location.replace("./app/home.html");
+    }else{
+	 window.location.replace("./web/home.html");
+	}
+</script>
